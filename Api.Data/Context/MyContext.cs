efc61358870852d0c48b269302ff194bae9c0405
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-05-13 17:33:23 -0300

    Adicionado a classe MyContext para vincular o BD

---
+++ Api.Data/Context/MyContext.cs
@@ -0,0 +1,19 @@
+﻿using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+
+    }
+}
