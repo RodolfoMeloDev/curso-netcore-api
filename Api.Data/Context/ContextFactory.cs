0f5f008d3820c381fe06dfef339a74220352a787
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-05-13 17:51:16 -0300

    Adicionado ContextFactory para criar o BD

---
+++ Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server=localhost;Port=5432;Database=dbAPI;User Id=postgres;Password=admin;Timeout=15;";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+    {
+    }
+}
