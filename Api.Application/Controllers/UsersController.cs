d69bf87ff2ec3e9498feb0d2056d4f3d3dc14486
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-05-16 20:56:37 -0300

    Iniciando a implementação dos Endpoints da Aplicação

---
+++ Api.Application/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Api.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromServices] IUserService service)
+        {
+            // aqui está válidando se os parametros da requisição são válidos
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await service.GetUsers());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
