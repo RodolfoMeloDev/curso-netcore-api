6a7e9d5e3dadfbcc64e59f45b0ee28fc219fff53
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-05-21 17:12:58 -0300

    Implementada a configuração do service na Aplicação para Login

--- Api.CrossCutting/DependencyInjection/ConfigureService.cs
+++ Api.CrossCutting/DependencyInjection/ConfigureService.cs
@@ -1,4 +1,5 @@
-﻿using Api.Domain.Interfaces.Services.User;
+﻿using Api.Domain.Interfaces.Services.Login;
+using Api.Domain.Interfaces.Services.User;
 using Api.Service.Services;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +13,7 @@ namespace Api.CrossCutting.DependencyInjection
             // Scooped = Cria uma nova instancia após 10 execuções daquele serviço
             // Singleton = Cria uma unica vez a instancia do service e sempre usará o mesmo na aplicação
             serviceCollection.AddTransient<IUserService, UserService>();
+            serviceCollection.AddTransient<ILoginService, LoginService>();
         }
     }
 }
