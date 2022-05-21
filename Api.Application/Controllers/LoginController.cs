31a3262c36c358f228e2e8a97ff82bcf0f26b807
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-05-21 16:57:00 -0300

    Adicionado LoginController

---
+++ Api.Application/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.Login;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Api.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<object> Login ([FromBody] UserEntity userEntity, [FromServices] ILoginService service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (userEntity == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var result = await service.FindByLogin(userEntity);
+
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+
+                return NotFound();
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
