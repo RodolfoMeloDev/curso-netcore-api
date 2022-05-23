5a88ae919fa3299c4094cf197be095c40edc564a
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-05-23 18:49:33 -0300

    Implementado classe LoginDto e substituido no método Login para utiliza-la

--- Api.Application/Controllers/LoginController.cs
+++ Api.Application/Controllers/LoginController.cs
@@ -1,4 +1,4 @@
-﻿using Api.Domain.Entities;
+﻿using Api.Domain.Dtos;
 using Api.Domain.Interfaces.Services.Login;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,21 +12,21 @@ namespace Api.Application.Controllers
     public class LoginController : ControllerBase
     {
         [HttpPost]
-        public async Task<object> Login ([FromBody] UserEntity userEntity, [FromServices] ILoginService service)
+        public async Task<object> Login ([FromBody] LoginDto loginDto, [FromServices] ILoginService service)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            if (userEntity == null)
+            if (loginDto == null)
             {
                 return BadRequest();
             }
 
             try
             {
-                var result = await service.FindByLogin(userEntity);
+                var result = await service.FindByLogin(loginDto);
 
                 if (result != null)
                 {
--- Api.Domain/Interfaces/Services/Login/ILoginService.cs
+++ Api.Domain/Interfaces/Services/Login/ILoginService.cs
@@ -1,10 +1,10 @@
-﻿using Api.Domain.Entities;
+﻿using Api.Domain.Dtos;
 using System.Threading.Tasks;
 
 namespace Api.Domain.Interfaces.Services.Login
 {
     public interface ILoginService
     {
-        Task<object> FindByLogin(UserEntity user);
+        Task<object> FindByLogin(LoginDto user);
     }
 }
