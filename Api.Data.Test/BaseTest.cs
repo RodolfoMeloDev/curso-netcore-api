2cea58eaccce8816f6460a7026ed42daf6dd6e8e
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-05-25 20:25:53 -0300

    Corrigido algumas versões de pacotes utilizados no projeto e criado projeto de Teste

---
+++ Api.Data.Test/BaseTest.cs
@@ -0,0 +1,45 @@
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
+
+namespace Api.Data.Test
+{
+    public abstract class BaseTest
+    {
+        public BaseTest()
+        {
+        }
+    }
+
+    public class DbTeste : IDisposable
+    {
+        private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public DbTeste()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<MyContext>(o =>
+                o.UseNpgsql($"Persist Securiry Info=True;Server=localhost;Port=5432;Database={dataBaseName};User Id=postgres;Password=admin;Timeout=15;"),
+                ServiceLifetime.Transient
+            );
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+    }
+
+}
