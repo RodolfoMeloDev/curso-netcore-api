87262644b686e25f2129257f2d10b6bba87461f2
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-05-26 20:29:38 -0300

    Adicionado Package Coverlet.Collector e Adicionado 1º Teste no projeto de serviços

---
+++ Api.Service.Test/Usuario/QuandoForExecutadoGet.cs
@@ -0,0 +1,38 @@
+﻿using Api.Domain.Dtos.User;
+using Api.Domain.Interfaces.Services.User;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.Usuario
+{
+    public class QuandoForExecutadoGet : UsuarioTestes
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName = "É possível Executar o Método GET.")]
+        public async Task E_Possivel_Executar_Metodo_Get()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(o => o.GetUser(IdUsuario)).ReturnsAsync(userDto);
+            _service = _serviceMock.Object;
+
+            var result = await _service.GetUser(IdUsuario);
+
+            Assert.NotNull(result);
+            Assert.True(result.Id == IdUsuario);
+            Assert.Equal(NomeUsuario, result.Name);
+
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(o => o.GetUser(It.IsAny<Guid>())).Returns(Task.FromResult((UserDto) null));
+            _service = _serviceMock.Object;
+
+            var _record = await _service.GetUser(IdUsuario);
+            Assert.Null(_record);
+        }
+    }
+}
