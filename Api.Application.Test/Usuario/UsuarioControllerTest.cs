a2c292e2d26ac1660152cd030c507edf2ff8b444
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-05-30 20:34:37 -0300

    Adicionado 1º teste unitário na controller de Usuario

---
+++ Api.Application.Test/Usuario/UsuarioControllerTest.cs
@@ -0,0 +1,92 @@
+﻿using Api.Application.Controllers;
+using Api.Domain.Dtos.User;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Application.Test.Usuario
+{
+    public class UsuarioControllerTest
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "É possível realizar o created.")]
+
+        public async Task E_Possivel_Invocar_a_Controller_Create()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Insert(It.IsAny<UserDtoCreate>())).ReturnsAsync(
+                new UserDtoCreateResult
+                {
+                    Id = Guid.NewGuid(),
+                    Name = nome,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow
+                });
+
+            _controller = new UsersController(serviceMock.Object);
+
+            Mock<IUrlHelper> url = new Mock<IUrlHelper>();
+            url.Setup(x => x.Link(It.IsAny<string>(), It.IsAny<object>())).Returns("http://localhost:5000");
+
+            _controller.Url = url.Object;
+
+            var userDtoCreate = new UserDtoCreate
+            {
+                Name = nome,
+                Email = email
+            };
+
+            var result = await _controller.InsertUser(userDtoCreate);
+            Assert.True(result is CreatedResult);
+
+            var resultValue = ((CreatedResult)result).Value as UserDtoCreateResult;
+            Assert.NotNull(resultValue);
+            Assert.Equal(userDtoCreate.Name, resultValue.Name);
+            Assert.Equal(userDtoCreate.Email, resultValue.Email);
+        }
+
+        [Fact(DisplayName = "Erro de BadRequest ModelState")]
+        public async Task Erro_BadRequest_ModelState()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Insert(It.IsAny<UserDtoCreate>())).ReturnsAsync(
+                new UserDtoCreateResult
+                {
+                    Id = Guid.NewGuid(),
+                    Name = nome,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow
+                });
+
+            _controller = new UsersController(serviceMock.Object);
+            _controller.ModelState.AddModelError("Name", "É um campo obrigatório");
+
+            Mock<IUrlHelper> url = new Mock<IUrlHelper>();
+            url.Setup(x => x.Link(It.IsAny<string>(), It.IsAny<object>())).Returns("http://localhost:5000");
+
+            _controller.Url = url.Object;
+
+            var userDtoCreate = new UserDtoCreate
+            {
+                Name = nome,
+                Email = email
+            };
+
+            var result = await _controller.InsertUser(userDtoCreate);
+            Assert.True(result is BadRequestResult);
+        }
+
+    }
+}
