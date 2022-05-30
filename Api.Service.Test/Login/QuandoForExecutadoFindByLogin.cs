cda22963058b2ca6468e36b789eac7ab9d6bc3c2
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-05-30 19:45:51 -0300

    Adicionado teste unitário para a classe Login

---
+++ Api.Service.Test/Login/QuandoForExecutadoFindByLogin.cs
@@ -0,0 +1,44 @@
+﻿using Api.Domain.Dtos;
+using Api.Domain.Interfaces.Services.Login;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.Login
+{
+    public class QuandoForExecutadoFindByLogin
+    {
+        private ILoginService _service;
+        private Mock<ILoginService> _serviceMock;
+
+        [Fact(DisplayName = "É possível executar o Método FindByLogin.")]
+        public async Task E_Possivel_Executar_Metodo_FindByLogin()
+        {
+            var email = Faker.Internet.Email();
+            var objetoRetorno = new
+            {
+                authenticated = true,
+                created = DateTime.UtcNow,
+                expiration = DateTime.UtcNow.AddHours(8),
+                accessToken = Guid.NewGuid(),
+                userName = Faker.Name.FullName(),
+                message = "Usuário Logado com sucesso"
+            };
+
+            var loginDto = new LoginDto
+            {
+                Email = email
+            };
+
+            _serviceMock = new Mock<ILoginService>();
+            _serviceMock.Setup(o => o.FindByLogin(loginDto)).ReturnsAsync(objetoRetorno);
+            _service = _serviceMock.Object;
+
+            var result = await _service.FindByLogin(loginDto);
+            Assert.NotNull(objetoRetorno);
+        }
+    }
+}
