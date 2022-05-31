ab04df2e80f7660a7f3ad58232fb6c36748048b5
Rodolfo Melo <rodolfo.melo.dev@gmail.com>
2022-05-30 21:20:07 -0300

    Adicionados os teste de Update, Delete e Get na controller de Usuário

--- Api.Application.Test/Usuario/UsuarioControllerTest.cs
+++ Api.Application.Test/Usuario/UsuarioControllerTest.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -54,8 +55,8 @@ namespace Api.Application.Test.Usuario
             Assert.Equal(userDtoCreate.Email, resultValue.Email);
         }
 
-        [Fact(DisplayName = "Erro de BadRequest ModelState")]
-        public async Task Erro_BadRequest_ModelState()
+        [Fact(DisplayName = "Erro de BadRequest ModelState Created.")]
+        public async Task Erro_BadRequest_ModelState_Created()
         {
             var serviceMock = new Mock<IUserService>();
             var nome = Faker.Name.FullName();
@@ -88,5 +89,202 @@ namespace Api.Application.Test.Usuario
             Assert.True(result is BadRequestResult);
         }
 
+        [Fact(DisplayName = "É possível realizar o Update.")]
+        public async Task E_Possivel_Invocar_a_Controller_Update()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Update(It.IsAny<UserDtoUpdate>())).ReturnsAsync(
+                new UserDtoUpdateResult
+                {
+                    Id = Guid.NewGuid(),
+                    Name = nome,
+                    Email = email,
+                    UpdateAt = DateTime.UtcNow
+                });
+
+            _controller = new UsersController(serviceMock.Object);
+
+            var userDtoUpdate = new UserDtoUpdate
+            {
+                Id = Guid.NewGuid(),
+                Name = nome,
+                Email = email
+            };
+
+            var result = await _controller.UpdateUser(userDtoUpdate);
+            Assert.True(result is OkObjectResult);
+        }
+
+        [Fact(DisplayName = "Erro de BadRequest ModelState Update.")]
+        public async Task Erro_BadRequest_ModelState_Update()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Update(It.IsAny<UserDtoUpdate>())).ReturnsAsync(
+                new UserDtoUpdateResult
+                {
+                    Id = Guid.NewGuid(),
+                    Name = nome,
+                    Email = email,
+                    UpdateAt = DateTime.UtcNow
+                });
+
+            _controller = new UsersController(serviceMock.Object);
+            _controller.ModelState.AddModelError("Name", "É um campo obrigatório");
+
+            var userDtoUpdate = new UserDtoUpdate
+            {
+                Id = Guid.NewGuid(),
+                Name = nome,
+                Email = email
+            };
+
+            var result = await _controller.UpdateUser(userDtoUpdate);
+            Assert.True(result is BadRequestResult);
+        }
+
+        [Fact(DisplayName = "É possível realizar o Delete.")]
+        public async Task E_Possivel_Invocar_a_Controller_Delete()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Delete(It.IsAny<Guid>())).ReturnsAsync(true);
+
+            _controller = new UsersController(serviceMock.Object);
+
+            var result = await _controller.DeleteUser(Guid.NewGuid());
+            Assert.True(result is OkObjectResult);
+        }
+
+        [Fact(DisplayName = "Erro de BadRequest ModelState Delete.")]
+        public async Task Erro_BadRequest_ModelState_Delete()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Delete(It.IsAny<Guid>())).ReturnsAsync(false);
+
+            _controller = new UsersController(serviceMock.Object);
+            _controller.ModelState.AddModelError("Id", "Formato Inválidos");
+
+            var result = await _controller.DeleteUser(Guid.NewGuid());
+            Assert.True(result is BadRequestResult);
+        }
+
+        [Fact(DisplayName = "É possível realizar o Get.")]
+        public async Task E_Possivel_Invocar_a_Controller_Get()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.GetUser(It.IsAny<Guid>())).ReturnsAsync(
+                new UserDto
+                {
+                    Id = Guid.NewGuid(),
+                    Name = nome,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow
+                });
+
+            _controller = new UsersController(serviceMock.Object);
+            var result = await _controller.GetUser(Guid.NewGuid());
+            Assert.True(result is OkObjectResult);
+        }
+
+        [Fact(DisplayName = "Erro de BadRequest ModelState Get.")]
+        public async Task Erro_BadRequest_ModelState_Get()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.GetUser(It.IsAny<Guid>())).ReturnsAsync(
+                new UserDto
+                {
+                    Id = Guid.NewGuid(),
+                    Name = nome,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow
+                });
+
+            _controller = new UsersController(serviceMock.Object);
+            _controller.ModelState.AddModelError("Id", "Formato Inválidos");
+
+            var result = await _controller.GetUser(Guid.NewGuid());
+            Assert.True(result is BadRequestResult);
+        }
+
+        [Fact(DisplayName = "É possível realizar o GetAll.")]
+        public async Task E_Possivel_Invocar_a_Controller_GetAll()
+        {
+            var serviceMock = new Mock<IUserService>();
+
+            serviceMock.Setup(m => m.GetUsers()).ReturnsAsync(
+                new List<UserDto>
+                {
+                    new UserDto
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = Faker.Name.FullName(),
+                        Email = Faker.Internet.Email(),
+                        CreateAt = DateTime.UtcNow
+                    },
+                    new UserDto
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = Faker.Name.FullName(),
+                        Email = Faker.Internet.Email(),
+                        CreateAt = DateTime.UtcNow
+                    }
+                });
+
+            _controller = new UsersController(serviceMock.Object);
+            var result = await _controller.GetUsers();
+
+            Assert.True(result is OkObjectResult);
+            
+            var resultValue = ((OkObjectResult) result).Value as IEnumerable<UserDto>;
+            Assert.True(resultValue.Count() == 2);
+        }
+
+        [Fact(DisplayName = "Erro de BadRequest ModelState GetAll.")]
+        public async Task Erro_BadRequest_ModelState_GetAll()
+        {
+            var serviceMock = new Mock<IUserService>();
+
+            serviceMock.Setup(m => m.GetUsers()).ReturnsAsync(
+                new List<UserDto>
+                {
+                    new UserDto
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = Faker.Name.FullName(),
+                        Email = Faker.Internet.Email(),
+                        CreateAt = DateTime.UtcNow
+                    },
+                    new UserDto
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = Faker.Name.FullName(),
+                        Email = Faker.Internet.Email(),
+                        CreateAt = DateTime.UtcNow
+                    }
+                });
+
+            _controller = new UsersController(serviceMock.Object);
+            _controller.ModelState.AddModelError("Id", "Formato Inválido");
+            var result = await _controller.GetUsers();
+
+            Assert.True(result is BadRequestObjectResult);
+        }
     }
 }
