48308e7fa1420fe8e98feb80263937f64af81d74
Rodolfo <brq.rodolfo@ao3tech.com>
2022-06-15 19:34:20 -0300

    Implementado os Repositorys para as entidades UF, CEP e Municipio

---
+++ Api.Data/Implementations/CepImplementation.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Data.Context;
+using Api.Data.Repository;
+using Api.Domain.Entities;
+using Api.Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Implementations
+{
+    public class CepImplementation : BaseRepository<CepEntity>, ICepRepository
+    {
+        private DbSet<CepEntity> _dataSet;
+        public CepImplementation(MyContext context) : base(context)
+        {
+            _dataSet = context.Set<CepEntity>();
+        }
+
+        public async Task<CepEntity> SelectAsync(string cep)
+        {
+            return await _dataSet.Include(c => c.Municipio)
+                                 .ThenInclude(m => m.Uf)
+                                 .FirstOrDefaultAsync(c => c.Cep.Equals(cep));
+        }
+    }
+}
