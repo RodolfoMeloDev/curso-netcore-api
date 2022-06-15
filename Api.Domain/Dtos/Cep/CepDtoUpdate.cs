1fd914dbb94882cbbd06efcc61a542813223c2e5
Rodolfo <brq.rodolfo@ao3tech.com>
2022-06-15 17:46:54 -0300

    Adicionado as DTOs de CEP, Municipio e UF

---
+++ Api.Domain/Dtos/Cep/CepDtoUpdate.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Dtos.Cep
+{
+    public class CepDtoUpdate
+    {
+        [Required(ErrorMessage = "Id é campo obrigatório")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "CEP é campo obrigatório")]
+        public string Cep { get; set; }
+
+        [Required(ErrorMessage = "Logradouro é campo obrigatório")]
+        public string Logradouro { get; set; }
+
+        public string Numero { get; set; }
+
+        [Required(ErrorMessage = "Municipío é campo obrigatório")]
+        public Guid MunicipioId { get; set; }
+    }
+}
