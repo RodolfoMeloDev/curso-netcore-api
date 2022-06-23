0c22a2192d4b152a661632ae42d2812a10e9a6c1
Rodolfo <brq.rodolfo@ao3tech.com>
2022-06-23 15:51:36 -0300

    Adicionado teste unitários para a cadama Service de UF

---
+++ Api.Service.Test/Uf/QuandoForExecutadoGet.cs
@@ -0,0 +1,37 @@
+﻿using Api.Domain.Dtos.Uf;
+using Api.Domain.Interfaces.Services.UF;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.Uf
+{
+    public class QuandoForExecutadoGet: UfTestes
+    {
+        private IUfService _service;
+        private Mock<IUfService> _serviceMock;
+
+        [Fact(DisplayName = "É Possível Executar o método Get.")]
+        public async Task Eh_Possivel_Executar_Metodo_Get()
+        {
+            _serviceMock = new Mock<IUfService>();
+            _serviceMock.Setup(m => m.Get(IdUf)).ReturnsAsync(UfDto);
+            _service = _serviceMock.Object;
+
+            var result = await _service.Get(IdUf);
+            Assert.NotNull(result);
+            Assert.True(result.Id == IdUf);
+            Assert.Equal(Nome, result.Nome);
+
+            _serviceMock = new Mock<IUfService>();
+            _serviceMock.Setup(m => m.Get(It.IsAny<Guid>())).Returns(Task.FromResult((UfDto)null));
+            _service = _serviceMock.Object;
+
+            var _record = await _service.Get(IdUf);
+            Assert.Null(_record);
+        }
+    }
+}
